8d836050fb881b199b9217b52089b8aa87ba21be
Wabiooo1 <105931424+Wabiooo1@users.noreply.github.com>
2025-09-21 00:54:21 -0500

    Delete cha directory

--- cha/config.py
+++
@@ -1,4 +0,0 @@
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'casino.db')
-SECRET_KEY = os.environ.get('SECRET_KEY', 'cambia_esto_por_una_clave_segura')
